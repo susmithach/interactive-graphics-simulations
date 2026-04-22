7eec5f66220667c5584448e48d660972b525605a
susmithachakka <susmithachakka123@gmail.com>
2026-04-22 17:42:05 -0500

    improved scene bounds and obstacle layout

--- term_project/main.py
+++ term_project/main.py
@@ -20,6 +20,11 @@ def is_colliding(pos_a, size_a, pos_b, size_b):
           abs(pos_a[2] - pos_b[2]) < (size_a[2] + size_b[2]) / 2
       )
 
+def is_inside_floor(pos, x_limit, z_limit):
+      return (
+          -x_limit <= pos[0] <= x_limit and
+          -z_limit <= pos[2] <= z_limit
+      )
 
 class Test(Base):
 
@@ -30,7 +35,8 @@ class Test(Base):
         self.scene = Scene()
         self.camera = Camera()
 
-        self.camera.setPosition(0, 2.5, 7)
+        self.camera.setPosition(1.8, 2.4, 7)
+        self.camera.rotateY(0.18)
         self.camera.rotateX(-0.35)
 
         base_dir = Path(__file__).resolve().parent
@@ -45,7 +51,7 @@ class Test(Base):
         self.player.setPosition(0, -0.4, 2)
         self.scene.add(self.player)
 
-        floor_geometry = RectangleGeometry(width=10, height=10)
+        floor_geometry = RectangleGeometry(width=10, height=12)
         floor_material = SurfaceBasicMaterial({"baseColor": [0.45, 0.45, 0.45]})
 
         self.floor = Mesh(floor_geometry, floor_material)
@@ -53,7 +59,7 @@ class Test(Base):
         self.floor.setPosition(0, -1, 0)
         self.scene.add(self.floor)
 
-        wall_geometry = BoxGeometry(width=3, height=1.5, depth=0.4)
+        wall_geometry = BoxGeometry(width=3, height=1.5, depth=0.7)
         wall_material = SurfaceBasicMaterial({"baseColor": [0.8, 0.15, 0.12]})
 
         self.wall = Mesh(wall_geometry, wall_material)
@@ -62,7 +68,9 @@ class Test(Base):
 
         self.player_speed = 0.05
         self.player_size = [1.2, 1.2, 1.2]
-        self.wall_size = [3, 1.5, 0.4]
+        self.wall_size = [3, 1.5, 0.7]
+        self.floor_x_limit = 4.4
+        self.floor_z_limit = 5.4
         
 
     def update(self):
@@ -85,12 +93,20 @@ class Test(Base):
             current_position[2] + dz
         ]
 
-        if not is_colliding(
+        blocked_by_wall = is_colliding(
             proposed_position,
             self.player_size,
             self.wall.getPosition(),
             self.wall_size
-        ):
+        )
+
+        inside_floor = is_inside_floor(
+            proposed_position,
+            self.floor_x_limit,
+            self.floor_z_limit
+        )
+
+        if inside_floor and not blocked_by_wall:
             self.player.setPosition(
                 proposed_position[0],
                 proposed_position[1],
